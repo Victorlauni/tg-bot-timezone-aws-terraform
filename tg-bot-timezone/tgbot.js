2de0ceaba007ff1a6777ebdd422b3d1b8b933807
Victor Lau <vtleluc@gmail.com>
2023-07-12 06:41:20 -0400

    config lambda to get telegram api context and reply

--- tg-bot-timezone/tgbot.js
+++ tg-bot-timezone/tgbot.js
@@ -10,11 +10,25 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.tg_bot_token;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token);
 
-module.exports.handler = async (event) => {
-  console.log('Event: ', event);
-  let responseMessage = 'Hello, World!';
+module.exports.handler = async (event, context, callback) => {
+  const { body } = event;
+  const bodyJson = JSON.parse(body);
+  console.debug('Event: ', event);
+  if (bodyJson.message) {
+    // Retrieve the ID for this chat
+    // and the text that the user sent
+    const { chat: { id }, text } = bodyJson.message;
+    
+    // Create a message to send back
+    // We can use Markdown inside this
+    const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+    // Send our new message back in Markdown and
+    // wait for the request to finish
+    await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+  }
 
   return {
     statusCode: 200,
@@ -22,7 +36,7 @@ module.exports.handler = async (event) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      message: responseMessage,
+      message: "OK",
     }),
   }
 }
